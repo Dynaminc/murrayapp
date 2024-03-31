08203b357147cfe6a853654007237374d849ea04
dynaminc <demilademuyiwa@gmail.com>
2024-03-31 22:04:36 +0100

    fix reversal

--- securities/urls.py
+++ securities/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path("", views.index, name="home"),
     path("api/managestrike", views.get_strike_breakdown),
     path("api/stocks/", views.stocks),
-    # path("api/test", views.test_end),
-    path("api/test", views.trigger_store),
+    path("api/test", views.test_end),
+    # path("api/test", views.trigger_store),
     path("api/clean", views.clean_end),
     path(
         "api/stocks/<str:symbol>/",
