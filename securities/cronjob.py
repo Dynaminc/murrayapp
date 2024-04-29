7e72afef2714101d9837771b2a145bb074cc8a34
dynaminc <demilademuyiwa@gmail.com>
2024-04-29 17:33:27 +0100

    Time test

--- securities/cronjob.py
+++ securities/cronjob.py
@@ -627,11 +627,11 @@ def generate_flow_combinations(current_datetime):
     previous, current, final = new_distinct_timestamps.index(timestamp) - 1,  new_distinct_timestamps.index(timestamp), new_distinct_timestamps.index(timestamp) + 1 
     print(new_distinct_timestamps.index(timestamp) - 1,  new_distinct_timestamps.index(timestamp), new_distinct_timestamps.index(timestamp) + 1 )
     
-    # stocks = [ StockSerializer(item).data for item in Stock.objects.filter(
-    #                                         date_time__gte=new_distinct_timestamps[final],
-    #                                         date_time__lt=(new_distinct_timestamps[final] + timedelta(minutes=1))).all()]
+    stocks = [ StockSerializer(item).data for item in Stock.objects.filter(
+                                            date_time__gte=new_distinct_timestamps[final] - timedelta(minutes=1),
+                                            date_time__lt=(new_distinct_timestamps[final] + timedelta(minutes=1))).all()]
     
-    stocks = [ StockSerializer(item).data for item in Stock.objects.order_by('-date_time')[:31]]
+    # stocks = [ StockSerializer(item).data for item in Stock.objects.order_by('-date_time')[:31]]
     print('Stocks', len(stocks))
     dataset = Combination.objects.filter(
         Q(date_time=new_distinct_timestamps[previous]) |
