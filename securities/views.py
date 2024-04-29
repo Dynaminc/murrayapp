6352112b6850d8877ddcdcb5a0b5f7e6a64093df
dynaminc <demilademuyiwa@gmail.com>
2024-04-29 16:47:37 +0100

    test

--- securities/views.py
+++ securities/views.py
@@ -450,7 +450,7 @@ def clean_comb():
     # return 'cleaned'
 
     count = 0 
-    times = [datetime(2024, 4, 29, 11, 0)]
+    times = [datetime(2024, 4, 29, 11, 40)]
     for item in times:
         print('Running clean module ')
         data = Combination.objects.filter(date_time__gte=item).all()
