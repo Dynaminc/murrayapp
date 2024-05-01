9ca2ad0644fbbb74491d835e76da7a08f2ffaa1a
dynaminc <demilademuyiwa@gmail.com>
2024-05-01 07:58:20 +0100

    exporting data

--- securities/assess.py
+++ securities/assess.py
@@ -252,7 +252,7 @@ def top_glow():
 
 def get_all_stocks():
     # Define the start time
-    start_time = datetime(2024, 4, 29, 10, 59)
+    start_time = datetime(2024, 4, 24, 11)
 
     # Fetch all Stock objects with date_time greater than or equal to start_time
     queryset = Stock.objects.filter(date_time__gte=start_time).order_by('-date_time')
