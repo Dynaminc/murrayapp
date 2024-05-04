ac211cbec13c205ee7a3db6194bd55d820453bb8
dynaminc <demilademuyiwa@gmail.com>
2024-05-04 07:53:18 +0100

    added min max

--- securities/simulator.py
+++ securities/simulator.py
@@ -45,45 +45,23 @@ def simulate_compute():
 def export_min_max():
     timestamp = datetime(2024, 4, 24, 11)
 
-    # Optimized filtering using Subquery and OuterRef
-    tmp_distinct_timestamps = Stock.objects.filter(
-        date_time__gte=timestamp
-    ).values(
-        "date_time",
-        min_score=Subquery(Combination.objects.filter(date_time=Q(**{"date_time": "date_time"})).values("avg").order_by("avg").first()),  # Use Q object
-        max_score=Subquery(Combination.objects.filter(date_time=Q(**{"date_time": "date_time"})).values("avg").order_by("-avg").first()),  # Use Q object
-    ).distinct().order_by("date_time")
-
+    print(data)
+    timestamp = datetime(2024, 4, 24, 11)
+    start_timestamp = datetime.strptime(str(timestamp), "%Y-%m-%d %H:%M:%S")
+    tmp_distinct_timestamps = [item['date_time'] for item in Stock.objects.filter(date_time__gte=timestamp).values("date_time").order_by("date_time").distinct()]
     data = []
-    for timepoint in tmp_distinct_timestamps:
-        short = timepoint["min_score"]  # Use pre-computed min score directly
-        long = timepoint["max_score"]  # Use pre-computed max score directly
-        info = {
-            "timestamp": timepoint["date_time"],
-            "short": short,
-            "max": short,  # Consistent naming (max score refers to short's score)
-            "long": long,
-            "min": long,  # Consistent naming (min score refers to long's score)
-        }
-        data.append(info)
-
-    # print(data)
-    # timestamp = datetime(2024, 4, 24, 11)
-    # start_timestamp = datetime.strptime(str(timestamp), "%Y-%m-%d %H:%M:%S")
-    # tmp_distinct_timestamps = [item['date_time'] for item in Stock.objects.filter(date_time__gte=timestamp).values("date_time").order_by("date_time").distinct()]
-    # data = []
-    # for timepoint in sorted(tmp_distinct_timestamps):
-    #     print(timepoint)
-    #     filtered_combinations = Combination.objects.filter(date_time=timepoint)
-    #     combs = [{'symbol':item.symbol,'score':item.avg,'date':timepoint} for item in filtered_combinations]
-    #     combs.sort(key=lambda x: x['score'], reverse=True)
-    #     try:
-    #         short = combs[0]['symbol']
-    #         long = combs[-1]['symbol']
-    #         info = {'timestamp':timepoint, 'short':short, 'max':combs[0]['score'],  'long': long, 'min': combs[-1]['score'] }        
-    #         data.append(info)
-    #     except Exception as E:
-    #         print(E)
+    for timepoint in sorted(tmp_distinct_timestamps):
+        print(timepoint)
+        filtered_combinations = Combination.objects.filter(date_time=timepoint)
+        combs = [{'symbol':item.symbol,'score':item.avg,'date':timepoint} for item in filtered_combinations]
+        combs.sort(key=lambda x: x['score'], reverse=True)
+        try:
+            short = combs[0]['symbol']
+            long = combs[-1]['symbol']
+            info = {'timestamp':timepoint, 'short':short, 'max':combs[0]['score'],  'long': long, 'min': combs[-1]['score'] }        
+            data.append(info)
+        except Exception as E:
+            print(E)
         
     df = pd.DataFrame(data)
     # Set open_time as index
