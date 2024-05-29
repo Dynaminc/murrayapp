2f9809549368d2c72ca9c4dd033ad235b723827e
dynaminc <demilademuyiwa@gmail.com>
2024-05-29 19:15:01 +0100

    Loader fix

--- securities/cronjob.py
+++ securities/cronjob.py
@@ -1036,8 +1036,8 @@ def new_flow_migrator(initial):
     print('Initiated')   
     
     count = 0 
-    initial_timestamp = datetime.strptime(str(Cronny.objects.latest('date_time').symbol), "%Y-%m-%d %H:%M:%S") # datetime(2024, 4,  29, 9,39 ) # # datetime(2024, 4,  30, 16) 
-    # initial_timestamp = datetime.strptime(str(initial), "%Y-%m-%d %H:%M:%S")
+    # initial_timestamp = datetime.strptime(str(Cronny.objects.latest('date_time').symbol), "%Y-%m-%d %H:%M:%S") # datetime(2024, 4,  29, 9,39 ) # # datetime(2024, 4,  30, 16) 
+    initial_timestamp = datetime.strptime(str(initial), "%Y-%m-%d %H:%M:%S")
     # datetime(2024, 4,  23, 10, 2)
     # initial_timestamp = datetime(2024, 4,  29, 11, )
     
