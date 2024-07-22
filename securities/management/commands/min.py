2bc01dd71ea4de74aed6b6cadcb5a286de5b3f0f
dynaminc <demilademuyiwa@gmail.com>
2024-07-22 07:43:06 -0400

    july 22

--- securities/management/commands/min.py
+++ securities/management/commands/min.py
@@ -1,5 +1,3 @@
-
-
 from django.core.management.base import BaseCommand
 from securities.models import Combination, Stock
 import json
--- securities/simulator.py
+++ securities/simulator.py
@@ -47,7 +47,7 @@ def export_min_max():
     # Define the timestamp
     # timestamp = datetime(2024, 5, 20, 11)
     # timestamp = datetime(2024, 6, 17, 11)
-    timestamp = datetime(2024, 7, 8)
+    timestamp = datetime(2024, 7, 16)
 
     # Fetch all combinations grouped by timestamp
     combinations = Combination.objects.filter(date_time__gte=timestamp)\
